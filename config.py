a4986846d0cc99ea86804e6720b69dcf479c4b33
Liu Kainan <146005327+Kainan-Liu@users.noreply.github.com>
2023-10-28 10:56:15 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,23 @@
+import torch
+
 CHECKPOINT = "./tmp" #load模型的路径
-EPOCHS = 100 # 训练每个模型要用的epoch
+EPOCHS = 200 # 训练每个模型要用的epoch
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # model1: nnRegression
 SPLIT_SIZE = 50
+
+filepath = {
+    "csv": {
+        "adata_obs_meta": './data/adata_obs_meta.csv',
+        "id_map": './data/id_map.csv',
+        "multiome_obs_meta": './data/multiome_obs_meta.csv',
+        "multiome_var_meta": './data/multiome_var_meta.csv',
+        "sample_submission": './data/sample_submission.csv'
+    },
+    "parquet": {
+        "adata_train": './data/adata_train.parquet',
+        "de_trian": "./data/de_train.parquet",
+        "multiome_train": "./data/multiome_train.parquet"  
+    }
+}
--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 import config
 import os
 import pandas as pd
+import numpy as np
 import torch
 import torch.nn as nn
 from sklearn.preprocessing import LabelEncoder
 from typing import Any
+from util import split
 
 
 
@@ -15,9 +17,10 @@ class Preprocess:
         if os.path.exists(train_data_path) and os.path.exists(test_data_path):
                 # train
                 self.train_data = pd.read_parquet(train_data_path)
-                features = ['cell_type','sm_name','sm_lincs_id','SMILES','control']
-                self.labels_train = self.train_data.drop(columns = features)
+                self.features = ['cell_type','sm_name','sm_lincs_id','SMILES','control']
+                self.labels_train = self.train_data.drop(columns = self.features)
                 self.features_train = self.train_data.iloc[:, :2]
+                self.positive_index = self.train_data[self.train_data['control'] == True].index
 
                 # test
                 self.id_map = pd.read_csv(test_data_path)
@@ -25,7 +28,7 @@ class Preprocess:
             raise FileNotFoundError(f"File not exists!")
         
 
-    def tokenize(self, vocab_size: None, hidden_size: int = 128):
+    def tokenize(self, hidden_size: int = 128, vocab_size: int = 0):
         '''
         :param vocab_size: the number of the cell/compound pairs
         :param hidden_size: the hidden_size of model
@@ -40,10 +43,10 @@ class Preprocess:
         ct = le.fit_transform(self.features_train['cell_type'])
         sm = le.fit_transform(self.features_train['sm_name'])
 
-        # step 1: # train
+        # train
         features_train_encoded = pd.DataFrame({'cell_type_encoded':ct,'sm_name_encoded':sm})
 
-        # step 1: # test
+        # test
         df = pd.concat([features_train_encoded, self.features_train],axis=1)
         cell_type_mapping = df.groupby(['cell_type_encoded', 'cell_type']).size().reset_index(name='count')
         sm_name_mapping = df.groupby(['sm_name_encoded', 'sm_name']).size().reset_index(name='count')
@@ -52,9 +55,10 @@ class Preprocess:
         self.id_map['cell_type'] = self.id_map['cell_type'].map(cell_type_mapping_dict)
         self.id_map['sm_name'] = self.id_map['sm_name'].map(sm_name_mapping_dict)
         features_test_encoded = self.id_map.iloc[:, 1:]
+        self.test_data = features_test_encoded.copy()
 
 
-        # step 2: combine two columns to get the union-id "2" + "116"-> "2116"
+        # step 2: combine two columns to get the union-id "2" + "116"-> "2116" 
         features_train_encoded["union_id"] = features_train_encoded["cell_type_encoded"].astype(str) + features_train_encoded["sm_name_encoded"].astype(str)
         features_train_encoded["union_id"] = features_train_encoded["union_id"].astype(int)
 
@@ -62,16 +66,69 @@ class Preprocess:
         features_test_encoded["union_id"] = features_test_encoded["union_id"].astype(int)
 
         # step 3: generate the embedding based on the union_id
-        if vocab_size is None:
+        if vocab_size == 0:
             vocab_size = max(features_train_encoded["union_id"]) + 2
         else:
             assert vocab_size > max(features_train_encoded["union_id"]) + 2
+
+        hidden_size = self.labels_train.shape[1]
 
         emb_func = nn.Embedding(num_embeddings=vocab_size, embedding_dim=hidden_size, padding_idx=0)
         train_embedding = emb_func(torch.LongTensor(features_train_encoded["union_id"]).reshape(-1, 1))
         test_embedding = emb_func(torch.LongTensor(features_test_encoded["union_id"]).reshape(-1, 1))
 
-        return train_embedding.squeeze(1).detach(), test_embedding.squeeze(1).detach()
+        return train_embedding.squeeze(1).detach().numpy(), test_embedding.squeeze(1).detach().numpy()
+    
+    def expand(self):
+        '''
+        use postive different expressional data to expand
+        '''
+        de_b, de_myeloid, de_nk, de_cd4, de_cd8, de_regt = 0, 0, 0, 0, 0, 0
+        de = {}
+
+        for id in self.positive_index:
+            if self.train_data["cell_type"][id] == "B cells":
+                de_b += self.labels_train.iloc[id, :].values / 2
+                de["B cells"] = de_b
+            elif self.train_data["cell_type"][id] == "Myeloid cells":
+                de_myeloid += self.labels_train.iloc[id, :].values / 2
+                de["Myeloid cells"] = de_myeloid
+            elif self.train_data["cell_type"][id] == "NK cells":
+                de_nk += self.labels_train.iloc[id, :].values / 2
+                de["NK cells"] = de_nk
+            elif self.train_data["cell_type"][id] == "T cells CD4+":
+                de_cd4 += self.labels_train.iloc[id, :].values / 2
+                de["T cells CD4+"] = de_cd4
+            elif self.train_data["cell_type"][id] == "T cells CD8+":
+                de_cd8 += self.labels_train.iloc[id, :].values / 2
+                de["T cells CD8+"] = de_cd8
+            elif self.train_data["cell_type"][id] == "T regulatory cells":
+                de_regt += self.labels_train.iloc[id, :].values / 2
+                de["T regulatory cells"] = de_regt
+            else:
+                raise ValueError("no such cell type")
+
+        self.expand_train_data = self.train_data.copy()
+        self.expand_test_data = self.test_data.copy()
+        self.expand_test_data = pd.concat((self.expand_test_data, pd.DataFrame(np.random.randn(self.expand_test_data.shape[0], self.labels_train.shape[1]))), axis=1)
+        cell_types = [types for types in self.expand_train_data["cell_type"].astype("category").cat.categories]
+        for cell_type in cell_types:
+            indices_train = self.expand_train_data[self.expand_train_data["cell_type"] == cell_type].index
+            self.expand_train_data.iloc[indices_train, 5:] = de[cell_type]
+            if cell_type in ["B cells", "Myeloid cells"]:
+                indices_test = self.expand_test_data[self.expand_test_data["cell_type"] == cell_type].index
+                self.expand_test_data.iloc[indices_test, 2:] = de[cell_type]
+            
+
+        self.expand_de = self.expand_train_data.drop(columns=self.features)
+        self.text_de = self.expand_test_data.drop(columns=["cell_type", "sm_name"])
+        return self.expand_de.to_numpy(), self.text_de.to_numpy()
+
+    
+    def split(self, split_size, loop):
+        sub_input, column_name = split(self.expand_de, config.SPLIT_SIZE, loop)
+        return sub_input, column_name
+
     
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         pass
